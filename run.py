5aa4bdf3b70bca95eb3d6196d194614e7d59ef25
SirMathhman <sirmathhman@gmail.com>
2022-10-06 21:41:09 -0400

    Update rum

--- run.py
+++ run.py
@@ -5,4 +5,6 @@ os.system("dotnet build")
 shutil.move(".\\Wormholes\\bin\\Debug\\Wormholes.dll",
             "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Dyson Sphere Program\\BepInEx\\plugins\\Wormholes.dll")
 
-print("Build successful.")
+print("Build successful, and mods moved.")
+
+os.system("\"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Dyson Sphere Program\\DSPGAME.exe\"")
