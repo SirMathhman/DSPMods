e989e2b2f39c9aad82030554cef9bed9143ab774
SirMathhman <sirmathhman@gmail.com>
2022-10-06 22:12:46 -0400

    ???

--- Wormholes/Plugin.cs
+++ Wormholes/Plugin.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using BepInEx;
 
 namespace Wormholes
 {
     [BepInPlugin("Wormholes", "Wormholes", "0.0")]
     internal class Plugin : BaseUnityPlugin
-    {   
+    {
         public void Awake()
         {
             Logger.LogInfo("Wormholes loading...");
+
+            var itemsProperty = typeof(LDB).GetProperty("items");
+            if (itemsProperty == null) return;
+            
+            var itemSet = (ItemProtoSet)itemsProperty.GetValue(null);
+            var items = new List<ItemProto>(itemSet.dataArray);
+            Logger.LogInfo(items.Count + " items in default game.");
+
+            var item = new ItemProto();
+            items.Add(item);
+            itemSet.dataArray = items.ToArray();
+            
+            itemsProperty.SetValue(null, itemSet);
+            Logger.LogInfo(LDB.items.Length + " items present in modded game.");
         }
     }
 }
